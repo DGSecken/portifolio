7441c87ea6541fdcf2b2ddeae2e07ed8b79372b2
Diogo Sela <diogosella@outlook.com>
2023-12-20 19:53:16 -0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal from './styles'
 import { Container } from './styles'
+import temaDark from './themes/dark'
+import temaLight from './themes/light'
+import { useState } from 'react'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 export default App
